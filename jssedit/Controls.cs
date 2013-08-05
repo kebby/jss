1da2115eb33fb352ab8660969deced6c201ea704
Tammo 'kb' Hinrichs <kb@kebby.org>
2013-08-05 19:32:21 +0200

    First attempt at user drawn controls... :)

---
+++ jssedit/Controls.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace jssedit
+{
+
+    class GraphControl : Control
+    {
+
+        public GraphControl()
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            // Call the OnPaint method of the base class.
+            base.OnPaint(pe);
+
+            var g = pe.Graphics;
+
+            // Declare and instantiate a new pen.
+            System.Drawing.Pen myPen = new System.Drawing.Pen(Color.White, 1);
+
+            // Draw an aqua rectangle in the rectangle represented by the control.
+            //g.DrawRectangle(myPen, new Rectangle(20, 20, 50, 50));
+
+
+            // Create a GraphicsPath object.
+            GraphicsPath myPath = new GraphicsPath();
+
+            // Set up and call AddArc, and close the figure.
+            Rectangle rect = new Rectangle(20, 20, 50, 100);
+            int cw = 10;
+            myPath.StartFigure();
+            myPath.AddLine(rect.Right, rect.Top + cw, rect.Right, rect.Bottom - cw);
+            myPath.AddArc(new Rectangle(rect.Right - cw, rect.Bottom - cw, cw, cw), 0, 90);
+            myPath.AddLine(rect.Right - cw, rect.Bottom, rect.Left + cw, rect.Bottom);
+            myPath.AddArc(new Rectangle(rect.Left, rect.Bottom - cw, cw, cw), 90, 90);
+            myPath.AddLine(rect.Left, rect.Bottom - cw, rect.Left, rect.Top + cw);
+            myPath.AddArc(new Rectangle(rect.Left, rect.Top, cw, cw), 180, 90);
+            myPath.AddLine(rect.Left + cw, rect.Top, rect.Right - cw, rect.Top);
+            myPath.AddArc(new Rectangle(rect.Right - cw, rect.Top , cw, cw), 270, 90);
+            myPath.CloseFigure();
+
+            // Draw the path to screen.
+
+            var br = new SolidBrush(Color.AliceBlue);
+
+            g.FillPath(br, myPath);
+            g.DrawPath(new Pen(Color.Red, 1), myPath);
+
+        }
+    }
+
+}
--- jssedit/Form1.Designer.cs
+++ jssedit/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new jssedit.GraphControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(815, 455);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 479);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GraphControl button1;
     }
 }
 
